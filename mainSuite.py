14bdfa07c90fabfd7da76a44679b9289834a397b
Namchuk-Maksym <98904412+Namchuk-Maksym@users.noreply.github.com>
2022-06-25 10:35:25 +0300

    Update mainSuite.py

--- mainSuite.py
+++ mainSuite.py
@@ -5,7 +5,7 @@ class SquareTest(unittest.TestCase):
         expected = 25
         square = main.Square(5).square()
     def test_square_2(self):
-        expected = None
+        expected = "Name cannot be lower then 0"
         square = main.Square(-1).square()    
         self.assertEqual(expected, square)
 class RectangleTest(unittest.TestCase):
@@ -14,7 +14,7 @@ class RectangleTest(unittest.TestCase):
         square = main.Rectangle(3,4).square()
         self.assertEqual(expected, square)
     def test_square_2(self):
-        expected = None
+        expected = "Name cannot be lower then 0"
         square = main.Rectangle(-1,4).square()
         self.assertEqual(expected, square)    
 class CircleTest(unittest.TestCase):    
@@ -23,7 +23,7 @@ class CircleTest(unittest.TestCase):
         square = main.Circle(10).square()
         self.assertEqual(expected,square)
     def test_square_2(self):
-        expected = None
+        expected = "Name cannot be lower then 0"
         square = main.Circle(-1).square()
         self.assertEqual(expected,square)    
 class DimondTest(unittest.TestCase):    
@@ -32,6 +32,6 @@ class DimondTest(unittest.TestCase):
         square = main.Dimond(5,3).square()
         self.assertEqual(expected, square)
     def test_square_2(self):
-        expected = None
+        expected = "Name cannot be lower then 0"
         square = main.Dimond(-5,2).square()
-        self.assertEqual(expected, square)    
+        self.assertEqual(expected, square)
