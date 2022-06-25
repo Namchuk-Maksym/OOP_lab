e8cad8d70b4ef1a1bd93c43bb5c4eecee69ad32f
Namchuk-Maksym <98904412+Namchuk-Maksym@users.noreply.github.com>
2022-06-25 09:52:10 +0300

    Update mainSuite.py

--- mainSuite.py
+++ mainSuite.py
@@ -1,22 +1,37 @@
 import unittest
 import main 
 class SquareTest(unittest.TestCase): 
-    def test_square(self):
+    def test_square_1(self):
         expected = 25
         square = main.Square(5).square()
+    def test_square_2(self):
+        expected = None
+        square = main.Square(-1).square()    
         self.assertEqual(expected, square)
 class RectangleTest(unittest.TestCase):
-    def test_square(self):
+    def test_square_1(self):
         expected = 12
         square = main.Rectangle(3,4).square()
         self.assertEqual(expected, square)
+    def test_square_2(self):
+        expected = None
+        square = main.Rectangle(-1,4).square()
+        self.assertEqual(expected, square)    
 class CircleTest(unittest.TestCase):    
-    def test_square(self):
+    def test_square_1(self):
         expected = 314
         square = main.Circle(10).square()
         self.assertEqual(expected,square)
+    def test_square_2(self):
+        expected = None
+        square = main.Circle(-1).square()
+        self.assertEqual(expected,square)    
 class DimondTest(unittest.TestCase):    
-    def test_square(self):
+    def test_square_1(self):
         expected = 7.5
         square = main.Dimond(5,3).square()
         self.assertEqual(expected, square)
+    def test_square_2(self):
+        expected = None
+        square = main.Dimond(-5,2).square()
+        self.assertEqual(expected, square)    
